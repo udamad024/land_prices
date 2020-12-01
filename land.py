05a094814037bb67a12053ff58a9aa6e3e6533dd
udamad <audara97@gmail.com>
2020-12-01 23:31:17 +0530

    Added the starting point as page 2 (?page=2), small change)

--- land.py
+++ land.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://ikman.lk/en/shops/my-colombo-properties'
+my_url = 'https://ikman.lk/en/shops/my-colombo-properties?page=2'
 
 uClient = uReq(my_url) #opens connection to URL, grabbing the page
 page_html = uClient.read() #Gets the HTML file from opened URL
