47a61a10e1745110aeee44bc170355921501ffc2
udamad <audara97@gmail.com>
2020-12-01 23:38:48 +0530

    Final State, completed with user_input

--- land.py
+++ land.py
@@ -1,23 +1,27 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://ikman.lk/en/shops/my-colombo-properties?page=2'
+def land_checks(num):
+  my_url = 'https://ikman.lk/en/shops/my-colombo-properties?page={n}'.format(n=num)
 
-uClient = uReq(my_url) #opens connection to URL, grabbing the page
-page_html = uClient.read() #Gets the HTML file from opened URL
-uClient.close() #Closes Connection
+  uClient = uReq(my_url) #opens connection to URL, grabbing the page
+  page_html = uClient.read() #Gets the HTML file from opened URL
+  uClient.close() #Closes Connection
 
-#HTML parsing
-page_soup = soup(page_html, "html.parser")
+  #HTML parsing
+  page_soup = soup(page_html, "html.parser")
 
-containers =  page_soup.findAll("div",{"class":"ui-item"})
+  containers =  page_soup.findAll("div",{"class":"ui-item"})
 
-for container in containers:
-  title_container = container.findAll("a",{"class":"item-title h4"})
-  product_name = title_container[0].text
+  for container in containers:
+    title_container = container.findAll("a",{"class":"item-title h4"})
+    product_name = title_container[0].text
 
-  price = container.findAll("p",{"class":"item-info"})
-  product_price = price[0].text
+    price = container.findAll("p",{"class":"item-info"})
+    product_price = price[0].text
 
-  print("Land Title: " + product_name)
-  print("Land Price: " + product_price)
+    print("Land Title: " + product_name)
+    print("Land Price: " + product_price)
+
+num = int(input("Please Enter Requried number: "))
+land_checks(num)
